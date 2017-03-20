5a8901fc2c5a9db9df9403aba2bf40b63409bfa2
yzb201159520 <yzb201159520@163.com>
2017-03-20 23:24:40 +0800

    firstHello
    firstHellofirstHello

---
+++ TestSpring/src/com/tomas/test/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.tomas.test;
+
+/**
+ * 
+ * @author thomas
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("HelloWorld!");
+	}
+
+}
