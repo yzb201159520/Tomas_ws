f1ad99d82dc13539bb5a405c90a3ccb46e9fbfd6
yzb201159520 <yzb201159520@163.com>
2017-03-26 11:21:27 +0800

    上传了项目的配置文件
    上传了项目的配置文件，使得在其他地方导入项目的时候可以被识别为一个project

--- TestSpring/src/com/tomas/web/controller/SpittleController.java
+++ TestSpring/src/com/tomas/web/controller/SpittleController.java
@@ -1,7 +1,5 @@
 package com.tomas.web.controller;
 
-import javax.websocket.server.PathParam;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
